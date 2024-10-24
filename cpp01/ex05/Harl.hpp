8b8195aefe61a51daaa9a32987325bfde06be2a5
misratasci <misratasci@gmail.com>
2024-10-24 18:34:11 +0300

    cpp01 done

---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,12 @@
+
+#include <string>
+
+class Harl {
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+	public:
+		void complain(std::string level);
+};
