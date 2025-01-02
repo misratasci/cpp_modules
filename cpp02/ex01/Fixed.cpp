26476531cc0e3bfc5715917bb15c08b103e4514b
misratasci <misratasci@gmail.com>
2025-01-02 23:42:12 +0300

    cpp02 devam

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -1,6 +1,7 @@
 
 #include "Fixed.hpp"
 #include <iostream>
+#include <math.h>
 
 Fixed::Fixed(): value(0) {
 	std::cout << "Default constructor called" << std::endl;
@@ -11,13 +12,23 @@ Fixed::Fixed(Fixed const &src) {
 	*this = src;
 }
 
+Fixed::Fixed(int const num) {
+	std::cout << "Int constructor called" << std::endl;
+	this->value = num << this->fractional_bits;
+}
+
+Fixed::Fixed(float const flo) {
+	std::cout << "Float constructor called" << std::endl;
+	this->value = roundf(flo * (1 << fractional_bits));
+}
+
 Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed& Fixed::operator=(Fixed const &obj) {
 	std::cout << "Copy assignment operator called" << std::endl;
-	this->value = obj.getRawBits();
+	this->value = obj.value;
 	return *this;
 }
 
@@ -28,4 +39,17 @@ int Fixed::getRawBits() const {
 
 void Fixed::setRawBits(int const raw) {
 	this->value = raw;
+}
+
+float Fixed::toFloat() const {
+	return (float)this->value / (float)(1 << fractional_bits);
+}
+
+int Fixed::toInt() const {
+	return this->value >> fractional_bits;
+}
+
+std::ostream& operator<<(std::ostream& o, Fixed const &obj) {
+	o << obj.toFloat();
+	return o;
 }
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -1,6 +1,8 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+#include <iostream>
+
 class Fixed {
 	private:
 		int value;
@@ -9,12 +11,18 @@ class Fixed {
 	public:
 		Fixed();
 		Fixed(Fixed const &src);
+		Fixed(int const num);
+		Fixed(float const flo);
 		~Fixed();
 
 		Fixed& operator=(Fixed const &obj);
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int	 toInt(void) const;
 
 };
+
+std::ostream& operator<<(std::ostream& o, Fixed const &obj);
 
 #endif
