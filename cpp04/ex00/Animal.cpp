0423a8e908228d9e0996c5bd1614d5ad2895518c
misratasci <misratasci@gmail.com>
2025-03-13 21:32:10 +0300

    started cpp04

---
+++ cpp04/ex00/Animal.cpp
@@ -0,0 +1,27 @@
+
+#include "Animal.hpp"
+
+Animal::Animal() : type("default") {
+    std::cout << "Animal default constructor called" << std::endl;
+    return ;
+}
+
+Animal::Animal(Animal const &src) {
+    std::cout << "Animal copy constructor called" << std::endl;
+    this->type = src.type;
+}
+
+Animal::~Animal() {
+    std::cout << "Animal destructor called" << std::endl;
+    return ;
+}
+
+Animal &Animal::operator=(Animal const &src) {
+    std::cout << "Animal assignment operator called" << std::endl;
+    this->type = src.type;
+    return *this;
+}
+
+void Animal::makeSound(void) {
+    std::cout << this->type << " makes a default sound" << std::endl;
+}
