93d870f803c9b9ac4d06436225ccbded99656477
misratasci <misratasci@gmail.com>
2025-03-13 15:52:35 +0300

    changess

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -14,7 +14,7 @@ class ClapTrap {
 		ClapTrap(ClapTrap const &src);
 		ClapTrap(std::string name);
 		~ClapTrap();
-		ClapTrap operator=(ClapTrap const &src);
+		ClapTrap &operator=(ClapTrap const &src);
 		void attack(const std::string& target);
 		void takeDamage(unsigned int amount);
 		void beRepaired(unsigned int amount);
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -9,4 +9,6 @@ int main() {
     a.attack("zeynep");
     b.beRepaired(3);
     c.takeDamage(2);
+    a.takeDamage(10);
+    a.takeDamage(1);
 }
