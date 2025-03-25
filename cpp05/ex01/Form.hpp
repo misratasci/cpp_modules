28a1b1abf5bb7221e1e7ccb8d30365849b9af227
misratasci <misratasci@gmail.com>
2025-03-25 17:46:09 +0300

    started ex01

---
+++ cpp05/ex01/Form.hpp
@@ -0,0 +1,36 @@
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <iostream>
+# include <exception>
+
+class Form {
+    private:
+        const std::string name;
+        bool is_signed;
+		const int sign_grade;
+		const int exec_grade;
+
+    public:
+        Form();
+        Form(const Form &src);
+		Form(const std::string name, const int sign_grade, const int exec_grade);
+        ~Form();
+        Form &operator=(const Form &src);
+        const std::string getName() const;
+        int getGrade() const;
+
+        class GradeTooHighException: public std::exception {
+            public:
+                virtual const char* what() const throw();
+        };
+        class GradeTooLowException: public std::exception {
+            public:
+                virtual const char* what() const throw();
+        };
+};
+
+std::ostream &operator<<(std::ostream &os, Form const &src);
+
+#endif
